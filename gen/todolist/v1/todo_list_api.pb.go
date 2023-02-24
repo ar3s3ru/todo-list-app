4a6e4ae6f4e9c7e969221a2dd3910fd15f43e1e5
Danilo Cianfrone <danilocianfr@gmail.com>
2023-02-24 18:01:37 +0100

    fix: make proto a buf mod

--- gen/todolist/v1/todo_list_api.pb.go
+++ gen/todolist/v1/todo_list_api.pb.go
@@ -7,7 +7,7 @@
 package todolistv1
 
 import (
-	_ "bitbucket.org/chronomics/todo-list-app/gen/google/api"
+	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
